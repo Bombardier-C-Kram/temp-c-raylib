9518e20ff68d2a0dc73c55fccb2dc28f63720961
Brian-ED <brianellingsgaard9@gmail.com>
2025-04-05 23:24:31 +0200

    cleaned files

--- compile-windows.sh
+++
@@ -1,8 +0,0 @@
-zig cc -shared \
-  -I./raylib/include/ \
-  -I./raylib/lib \
-  -lraylib \
-  -L./raylib/lib \
-  src/temp-c-raylib.c \
-  -o temp-c-raylib.dll \
-  -target x86_64-windows \
--- compile.sh
+++
@@ -1,1 +0,0 @@
-zig cc -o libtemp-c-raylib.so --shared -I./raylib/lib -lraylib ./src/temp-c-raylib.c
