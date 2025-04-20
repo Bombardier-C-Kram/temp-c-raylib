9f27cbd5b8535ab0f2f6d5265fa8e2d1acc19b03
Brian-ED <brianellingsgaard9@gmail.com>
2025-04-20 20:37:13 +0200

    fixed path being personal & so file being symlink

--- runNewMakeRaylib.sh
+++ runNewMakeRaylib.sh
@@ -1,13 +1,15 @@
+#!/usr/bin/env bash
 if ! ( [ -e "raylib-source.tar.gz" ] || [ -e "raylib-5.5" ] ); then
   curl -L https://github.com/raysan5/raylib/archive/refs/tags/5.5.tar.gz --output raylib-source.tar.gz
   tar -xzf raylib-source.tar.gz
 fi
-
-/usr/bin/dyalogscript "/home/brian/Persinal/Scripts/APL/temp-c-raylib/src/convert_pointerArgs.apls"
+cd src
+/usr/bin/dyalogscript "convert_pointerArgs.apls"
+cd ..
 cd raylib-5.5/src
 make clean
 echo '#include "../../src/temp-c-raylib.c"' >> rcore.c
 make RAYLIB_LIBTYPE=SHARED $@
 sed -i '$ d' rcore.c
-cp -f libraylib.so ../../libtemp-c-raylib.so
+cp -f libraylib.so.550 ../../libtemp-c-raylib.so
 cd ../../
