fdf4aeac76494faeff0f43d9c25f9ba2c4550cd0
Brian-ED <brianellingsgaard9@gmail.com>
2025-06-21 20:39:29 +0100

    Now compiling with float convertion functions Needed for raylib-apl stack conversion branch

--- src/temp-c-raylib.c
+++ src/temp-c-raylib.c
@@ -24,6 +24,12 @@ DECLARE(int GetStructNameLength(int index)){
   while (name[acc] != '\0') acc++;
   return acc;
 }
+DECLARE(void FloatToDouble(double *n1, float *n2)){
+  *n1 = *n2;
+}
+DECLARE(void DoubleToFloat(float *n1, double *n2)){
+  *n1 = *n2;
+}
 DECLARE(void InitWindowRetPtr(int *width,  int *height,  const char **title)){InitWindow(*width, *height, *title);}  // Initialize window and OpenGL context
 DECLARE(void CloseWindowRetPtr()){CloseWindow();}                                     // Close window and unload OpenGL context
 DECLARE(void WindowShouldCloseRetPtr(bool *ret)){*ret=WindowShouldClose();}                               // Check if application should close (KEY_ESCAPE pressed or windows close icon clicked)
